7a8f2432f15a64403940c90c0d4a9daed179fd5a
Joel AlejandroFT <alexfrutos211@gmail.com>
2026-05-04 10:52:49 +0200

    tablas

--- frances.sql
+++ frances.sql
@@ -11,3 +11,18 @@ CREATE TABLE usuarios (
     TELEFONO CHAR(9) NOT NULL COMMENT 'Telefono movil del usuario',
     INSTITUTO VARCHAR(50) NULL COMMENT 'Instituto al que asiste el usuario'
 );
+CREATE TABLE Grupo (
+    Id_Grupo INT(10) AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Centro VARCHAR(64) NOT NULL,
+    Tutor INT(10) NOT NULL,
+    FOREIGN KEY (Tutor) REFERENCES Usuarios(Id_Usuario)
+);
+
+CREATE TABLE Alumno_Grupo (
+    Id_Usuario INT(10),
+    Id_Grupo INT(10),
+    PRIMARY KEY (Id_Usuario, Id_Grupo),
+    FOREIGN KEY (Id_Usuario) REFERENCES Usuarios(Id_Usuario),
+    FOREIGN KEY (Id_Grupo) REFERENCES Grupo(Id_Grupo)
+);
