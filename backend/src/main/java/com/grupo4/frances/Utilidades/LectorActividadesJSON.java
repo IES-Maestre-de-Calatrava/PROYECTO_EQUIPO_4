c3f386cec1530ffa13669965c653641a81623f5b
samudt24 <samudt4721@gmail.com>
2026-05-05 22:09:58 +0200

    Create LectorActividadesJSON.java

---
+++ backend/src/main/java/com/grupo4/frances/Utilidades/LectorActividadesJSON.java
@@ -0,0 +1,31 @@
+package com.grupo4.frances.Utilidades;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class LectorActividadesJSON<E> {
+
+	private String filename;
+	
+	public LectorActividadesJSON(String f) {
+		this.filename=f;
+	}
+	
+	public void guardar(ArrayList<E> lista) {
+		
+		try(ObjectOutputStream fich = new ObjectOutputStream
+				(new FileOutputStream(this.filename))){
+			
+			for (E obj : lista) {
+				fich.writeObject(obj);
+			}
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+}
