7f11110c4cfa54bb02ee053f53f5717ff30c2720
ahontt <165604078+ahontt@users.noreply.github.com>
2026-05-05 21:52:59 +0200

    Arreglo en Numericas.java y creacion de Seguridad.java
    Se convierte el metodo generarNumAleatorio en estatica, y se crea la clase Seguridad, que contiene el metodo generarClaveGrupo, que genera una clave de grupo aleatoria con formato [A-Z]{4}-[0-9]{4}

--- backend/src/main/java/com/grupo4/frances/Utilidades/Numericas.java
+++ backend/src/main/java/com/grupo4/frances/Utilidades/Numericas.java
@@ -1,11 +1,11 @@
 package com.grupo4.frances.Utilidades;
 
 public class Numericas {
-    public int generarNumAleatorio(int min, int max) {
+    public static int generarNumAleatorio(int min, int max) {
         double f = Math.random()/Math.nextDown(1.0);
         double x = min*(1.0 - f) + max*f;
 
         return (int) x;
     }
-    
+
 }
---
+++ backend/src/main/java/com/grupo4/frances/Utilidades/Seguridad.java
@@ -0,0 +1,18 @@
+package com.grupo4.frances.Utilidades;
+
+import static com.grupo4.frances.Utilidades.Numericas.*;
+
+public class Seguridad {
+    public static String generarClaveGrupo() {
+        StringBuilder clave = new StringBuilder();
+        for(int i = 0; i < 4; i++){
+            clave.append((char) generarNumAleatorio(65, 90));
+        }
+        clave.append('-');
+        for(int i = 0; i < 4; i++){
+            clave.append(generarNumAleatorio(0, 9));
+        }
+
+        return clave.toString();
+    }
+}
