a3e86b5bec826e66ab7578e2317907342db35035
samudt24 <samudt4721@gmail.com>
2026-05-05 11:00:42 +0200

    controller profesor

---
+++ backend/src/main/java/com/grupo4/frances/Controllers/ProfesorController.java
@@ -0,0 +1,49 @@
+package com.grupo4.frances.Controllers;
+
+import com.grupo4.frances.DTO.ProfesorDTO;
+import com.grupo4.frances.Exceptions.ProfesorNotFoundException;
+import com.grupo4.frances.Mappers.ProfesorMapper;
+import com.grupo4.frances.Repositories.ProfesorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/profesor") // Ruta actualizada a /actividad
+public class ProfesorController {
+
+    @Autowired
+    private ProfesorRepository repository;
+
+    /**
+     * Obtener todas las actividades
+     * @return List de ActividadDTO
+     */
+    @GetMapping("/find")
+    public List<ProfesorDTO> getAllActividades() {
+        return repository.findAll()
+                .stream()
+                .map(ProfesorMapper::toDTO)
+                .toList();
+    }
+
+    /**
+     * Obtener actividad por ID
+     * @param profesorId
+     * @return ProfesorDTO
+     * @throws ProfesorNotFoundException
+     */
+    @GetMapping("/find/{id}")
+    public ResponseEntity<ProfesorDTO> getProfesorByID(@PathVariable(value = "id") Long profesorId)
+            throws ProfesorNotFoundException {
+
+        ProfesorDTO profesor = repository.findById(profesorId)
+                .map(ProfesorMapper::toDTO)
+                .orElseThrow(() -> new ProfesorNotFoundException(profesorId));
+
+        return ResponseEntity.ok(profesor);
+    }
+}
---
+++ backend/src/main/java/com/grupo4/frances/Repositories/ProfesorRepository.java
@@ -0,0 +1,15 @@
+package com.grupo4.frances.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.grupo4.frances.persistence.Profesor;
+
+public interface ProfesorRepository extends JpaRepository<Profesor,List> {
+    @Query("SELECT c FROM Grupo c WHERE c.nombre = :nombre")
+    List<Profesor> buscarPorNombre(@Param("nombre") String nombre);
+
+}
