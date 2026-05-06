336b3f0c8f2ebc811081d7b27f8c02d1fbd85f2a
ahontt <165604078+ahontt@users.noreply.github.com>
2026-05-06 13:25:44 +0200

    Arreglar corsconfig

--- backend/src/main/java/com/grupo4/frances/CorsConfig.java
+++ backend/src/main/java/com/grupo4/frances/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.grupo4.frances;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
