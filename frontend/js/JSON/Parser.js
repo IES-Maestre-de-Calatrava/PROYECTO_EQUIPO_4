c468be0e5448a7662d2a5dad61fa51b2b3387edc
ahontt <165604078+ahontt@users.noreply.github.com>
2026-05-06 13:38:05 +0200

    Implementación de los parser para preguntas y respuestas de las actividades

--- frontend/js/JSON/Parser.js
+++ frontend/js/JSON/Parser.js
@@ -1,2 +1,25 @@
-// Placeholder
+export function parserPreguntas(stringJson) {
+    try {
+        const datos = JSON.parse(stringJson).preguntas;
+        let preguntas = [];
+        datos.forEach((p, index) => {
+            preguntas.push(p);
+        });
+        return preguntas;
+    } catch (e) {
+        console.log(`Se ha producido un error al parsear el json, stack: ${e}`);
+    } 
+}
 
+export function parserRespuestas(stringJson) {
+    try {
+        const datos = JSON.parse(stringJson).respuestas;
+        let respuestas = [];
+        datos.forEach((p, index) => {
+            respuestas.push(p);
+        });
+        return respuestas;
+    } catch (e) {
+        console.log(`Se ha producido un error al parsear el json, stack: ${e}`);
+    } 
+}
