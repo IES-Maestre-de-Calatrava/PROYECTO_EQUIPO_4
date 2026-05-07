9e354965c5149ece717170d3a7de8c214c71820a
ahontt <165604078+ahontt@users.noreply.github.com>
2026-05-07 12:12:40 +0200

    Añadir generador de UUID para gestión de sesiones

--- frontend/js/Utilidades.js
+++ frontend/js/Utilidades.js
@@ -15,3 +15,12 @@ export async function sha256(message) {
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
 }
+
+/*
+Funcion que genera un UUIDV4 para crear sesiones unicas para el usuario
+*/
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
+}
